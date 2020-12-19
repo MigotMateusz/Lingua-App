272e0b6e25c1ac88065a5b2d2fc465d6d86b6a83
Mateusz Migot <migotmateusz@gmail.com>
2020-12-19 14:28:01 +0100

    changed login layout and added ability to login

--- app/src/main/java/pl/mateuszmigot/linguaapp/LoginActivity.kt
+++ app/src/main/java/pl/mateuszmigot/linguaapp/LoginActivity.kt
@@ -4,6 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
@@ -15,6 +20,16 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         mAuth = FirebaseAuth.getInstance()
+
+        val loginButton = findViewById<MaterialButton>(R.id.LoginButton)
+        val forgotButton = findViewById<Button>(R.id.ForgetPassButton)
+        val signUpButton = findViewById<Button>(R.id.SignUpButton)
+
+
+        loginButton.setOnClickListener { login() }
+        //signUpButton.setOnClickListener { changeActivity(RegisterActivity::class.java) }
+        //forgotButton.setOnClickListener { changeActivity(ForgotPasswordActivity::class.java) }
+
     }
 
     override fun onStart() {
@@ -29,4 +44,25 @@ class LoginActivity : AppCompatActivity() {
         startActivity(intent)
         overridePendingTransition(R.anim.pull_in_left, R.anim.pull_out_right)
     }
+
+    private fun signIn(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+           .addOnCompleteListener(this) { task ->
+               if(task.isSuccessful) {
+                    changeActivity(MainActivity::class.java)
+               } else {
+                   Toast.makeText(baseContext, "Authentication failed.",
+                       Toast.LENGTH_SHORT).show()
+               }
+           }
+    }
+
+    fun login() {
+        val loginInput = findViewById<TextInputEditText>(R.id.LoginInput)
+        val passwordInput = findViewById<TextInputEditText>(R.id.PasswordInput)
+        val login = loginInput.text.toString()
+        val password = passwordInput.text.toString()
+        signIn(login, password)
+    }
+
 }
